0ae19510acd3264143af87a793e7ecb97ec4549f
inhasid <sidorchuk.inga@gmail.com>
2024-01-06 13:07:10 +0100

    feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,13 @@
+import './app.css';
+
+import Feedback from './Feedback/Feedback';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="divWrap">
+      <Feedback />
     </div>
   );
 };
+
+export default App;
